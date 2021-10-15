3ec07d449ef59a40717e4f4566292258b34597f3
ar[arnau321 <arnau123@att.net>
2021-10-15 13:20:55 -0400

    Fix bug

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const app = require('express')()
-const server = require('http').createServer(app)
-const cors = require('cors')
+const app = require('express')() 
+const server = require('http').createServer(app) 
+const cors = require('cors') 
 
+// set cors with socket io
+const io = require( 'socket.io')(server, {
+	cors: {
+		origin:  '*',
+		methods: ['GET',  'POST']
+	}
+}) 
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    method: ['GET', 'POST']
-  }
-})
-
-app.use(cors())
-
-const PORT = process.env.PORT || 5000
+app.use(cors()) 
+// set up PORT
+const PORT = process.env.PORT || 5000 
 
 app.get('/', (req, res) => {
-  res.send('Server is running')
-})
+	res.send('Running') 
+}) 
 
-io.on('connection', (socket) =>{
-  socket.emit('me', socket.id)
+io.on('connection', (socket) => {
+	socket.emit( 'me', socket.id) 
 
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('Call ended')
-  })
+	socket.on('disconnect', () => {
+		socket.broadcast.emit( 'callEnded')
+	}) 
+  // sends data to call user
+	socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit('callUser', { signal: signalData, from, name }) 
+	}) 
+  // listens for answered call from client
+	socket.on( 'answerCall', (data) => {
+		io.to(data.to).emit('callAccepted', data.signal)
+	}) 
+}) 
 
-  socket.on('calluser', ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit('calluser', {signal: signalData, from, name})
-  })
-
-  socket.on('answercall', (data) => {
-    io.to(data.to).emit('Call accepted.', data.signal)
-  })
-})
-
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`)) 
